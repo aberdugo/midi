f3d0de93e378608457fd5c4fcb3b4bfe0c65acc5
aberdugo <aberdugo@emergya.com>
2017-12-04 18:27:00 +0100

    Changes for test float for div #teclado (css/midi-box.css and midi-box.tpl.php)

--- midi-box.tpl.php
+++ midi-box.tpl.php
@@ -7,5 +7,9 @@
 ?>
 <div id="midi-box">
     <div id="buttons"></div>
-    <div id="pianoroll"></div>
+    <div id="div-midi-overflow">
+        <!-- <div id="teclado"></div> -->
+        <div id="pianoroll"></div>
+        <!-- <div id="div-midi-clear"></div> -->
+    </div>
 </div>
